fc3e98dea61fa0cacd40da8a0dbd8e56041d61d8
bear-zd <a827569457@163.com>
2023-10-10 19:37:11 +0800

    10.10

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,7 +13,6 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
 
 // Put your function here!
 // fn calculate_price_of_apples {
@@ -21,6 +20,7 @@
 // Don't modify this function!
 #[test]
 fn verify_test() {
+    fn calculate_price_of_apples(number:i32)->i32{let cost:i32 = if number<=40{number*2} else {number};cost}
     let price1 = calculate_price_of_apples(35);
     let price2 = calculate_price_of_apples(40);
     let price3 = calculate_price_of_apples(41);
