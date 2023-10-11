d206cde840198b3221c3844685ca42e256f0d21f
bear-zd <a827569457@163.com>
2023-10-11 16:24:02 +0800

    so hard

--- exercises/tests/tests7.rs
+++ exercises/tests/tests7.rs
@@ -34,8 +34,6 @@
 // Execute `rustlings hint tests7` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 fn main() {}
 
 #[cfg(test)]
@@ -46,7 +44,7 @@ mod tests {
     fn test_success() {
         let timestamp = std::time::SystemTime::now()
             .duration_since(std::time::UNIX_EPOCH)
-            .unwrap()
+            .unwrap() 
             .as_secs();
         let s = std::env::var("TEST_FOO").unwrap();
         let e: u64 = s.parse().unwrap();
